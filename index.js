a67776d8ece2586985f6d83acd986aafd2956b0f
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-09-24 00:22:42 +0500

    Added Data from local Storage to Table

--- index.js
+++ index.js
@@ -10,18 +10,32 @@ let desc=document.getElementById('description').value;
 if(localStorage.getItem('jsonitems')==null)
 {
     let arr=[];
-    arr.push(tit,desc);
-
+    arr.push([tit,desc]);
     localStorage.setItem('jsonitems',JSON.stringify(arr));
 }
 
 else
 {
    let arrstring=localStorage.getItem('jsonitems');
-   let arr2=JSON.parse(arrstring);
-
-    arr2.push(tit,desc);
-    localStorage.setItem('jsonitems',JSON.stringify(arr2));
+   arr=JSON.parse(arrstring);
+   arr.push([tit,desc]);
+   localStorage.setItem('jsonitems',JSON.stringify(arr));
 }
+// Adding Data from local Storage to Table(Populating the table)
 
-})
+let tablebody = document.getElementById("tablebody");
+let str="";
+
+arr.forEach((element,index) => {
+str +=`
+<tr>
+<th scope="row">${index + 1}</th>
+<td>${element[0]}</td>
+<td>${element[1]}</td> 
+<td> <button class="btn btn-primary btn-sm">Delete</button> </td> 
+</tr>
+`  
+});
+
+tablebody.innerHTML=str;
+});
