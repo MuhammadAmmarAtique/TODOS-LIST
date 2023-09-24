fbf3c64252ce24b3fa30492f2fa61a1739c128f6
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-09-24 14:44:24 +0500

    Activated delete button but it has bug right now

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 // Adding Click Event in "Add to List" button
 
-let addbtn=document.getElementById('add');
-
 function update ()
 {
     let tit=document.getElementById('title').value;
@@ -32,7 +30,7 @@ function update ()
     <th scope="row">${index + 1}</th>
     <td>${element[0]}</td>
     <td>${element[1]}</td> 
-    <td> <button class="btn btn-primary btn-sm">Delete</button> </td> 
+    <td> <button class="btn btn-primary btn-sm" onclick="remove(${index})">Delete</button> </td> 
     </tr>
     `  
     });
@@ -40,5 +38,20 @@ function update ()
     tablebody.innerHTML=str;
 }
 
+let addbtn=document.getElementById('add');
 addbtn.addEventListener('click',update);
 update();
+
+
+// Activating delete button from "your items section"
+function remove (index)
+{
+    console.log("deleted list item:",index+1);
+
+    let arrstring=localStorage.getItem('jsonitems');
+    arr=JSON.parse(arrstring);
+    // Deleting given index element from the array using splice()
+    arr.splice(index,1);
+    localStorage.setItem('jsonitems',JSON.stringify(arr));
+    update();
+}
