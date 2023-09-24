39c6bf596298d608fc8dac86d3727672b02dcd0d
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-09-24 13:36:17 +0500

    Added update( ) to update list,whenever usercomes

--- index.js
+++ index.js
@@ -2,40 +2,43 @@
 
 let addbtn=document.getElementById('add');
 
-addbtn.addEventListener('click',()=>
+function update ()
 {
-let tit=document.getElementById('title').value;
-let desc=document.getElementById('description').value;
+    let tit=document.getElementById('title').value;
+    let desc=document.getElementById('description').value;
+        
+    if(localStorage.getItem('jsonitems')==null)
+    {
+        let arr=[];
+        arr.push([tit,desc]);
+        localStorage.setItem('jsonitems',JSON.stringify(arr));
+    }
     
-if(localStorage.getItem('jsonitems')==null)
-{
-    let arr=[];
-    arr.push([tit,desc]);
-    localStorage.setItem('jsonitems',JSON.stringify(arr));
+    else
+    {
+       let arrstring=localStorage.getItem('jsonitems');
+       arr=JSON.parse(arrstring);
+       arr.push([tit,desc]);
+       localStorage.setItem('jsonitems',JSON.stringify(arr));
+    }
+    // Adding Data from local Storage to Table(Populating the table)
+    
+    let tablebody = document.getElementById("tablebody");
+    let str="";
+    
+    arr.forEach((element,index) => {
+    str +=`
+    <tr>
+    <th scope="row">${index + 1}</th>
+    <td>${element[0]}</td>
+    <td>${element[1]}</td> 
+    <td> <button class="btn btn-primary btn-sm">Delete</button> </td> 
+    </tr>
+    `  
+    });
+    
+    tablebody.innerHTML=str;
 }
 
-else
-{
-   let arrstring=localStorage.getItem('jsonitems');
-   arr=JSON.parse(arrstring);
-   arr.push([tit,desc]);
-   localStorage.setItem('jsonitems',JSON.stringify(arr));
-}
-// Adding Data from local Storage to Table(Populating the table)
-
-let tablebody = document.getElementById("tablebody");
-let str="";
-
-arr.forEach((element,index) => {
-str +=`
-<tr>
-<th scope="row">${index + 1}</th>
-<td>${element[0]}</td>
-<td>${element[1]}</td> 
-<td> <button class="btn btn-primary btn-sm">Delete</button> </td> 
-</tr>
-`  
-});
-
-tablebody.innerHTML=str;
-});
+addbtn.addEventListener('click',update);
+update();
